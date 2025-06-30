7b210d741b29ef69def648ea5ef64d0ee45bc840
ayanyuegupta <ayanyuegupta@hotmail.co.uk>
2025-07-01 00:19:36 +0200

    updates

--- template.js
+++ template.js
@@ -2,16 +2,21 @@ function loadTemplate(title, content) {
     document.title = title;
     document.body.innerHTML = `
         <header>
-            <h1><a href="index.html">AI Gaza Stories</a></h1>
-        </header>
+	    <div class="container">
+                <h1>AI Gaza Stories</h1>
+            </div>
+	</header>
 
         <!-- navigation--> 
         <div class="navigation">
-            <a href="/conditions.html">Conditions</a>
-            <a href="/leaders.html">Leaders</a>
-            <a href="/workers.html">Workers</a>
-            <a href="/notes.html">Notes</a>
-        </div>
+	    <div class="container">
+	        <a href="index.html">Home</a>
+                <a href="/conditions.html">Conditions</a>
+                <a href="/leaders.html">Leaders</a>
+                <a href="/workers.html">Workers</a>
+                <a href="/notes.html">Notes</a>
+            </div>
+	</div>
         
 	<main>
 	    ${content}
